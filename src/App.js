58ce5ebfa3c5a0b5b49bc8e221bc24636c06d4e8
unknown <binyaalex@gmail.com>
2022-07-10 12:40:54 +0300

    made specialists

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Pregnancy from './components/servicesList/Pregnancy'
+import Doctors from './components/doctors/Doctors';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <BrowserRouter>
           <Routes>
               <Route path="/" element={<Pregnancy />} />
+              <Route path="/specialists" element={<Doctors />} />
           </Routes>
         </BrowserRouter>
       </div>
